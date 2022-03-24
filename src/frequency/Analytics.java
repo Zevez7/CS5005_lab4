3474cefe2039da4a88288b5976143c29ecf65658
Zevez7 <datguy7@gmail.com>
2022-03-24 16:07:52 -0400

    fix the 0.1 and 0.2

--- src/frequency/Analytics.java
+++ src/frequency/Analytics.java
@@ -15,20 +15,20 @@ public class Analytics {
 
     // remove all non letters, lowercase all words, split words by white space
     String[] wordsArray = message.replaceAll("[^a-zA-Z ]", "").toUpperCase().split("\\s+");
-    System.out.println(Arrays.toString(wordsArray));
+//    System.out.println(Arrays.toString(wordsArray));
 
     Map<String, Double> hMap = new HashMap<>();
     // loop through words
     for (String word : wordsArray) {
-      System.out.println(word);
+//      System.out.println(word);
 
       // check to see if word is already in Hashmap
       if (hMap.containsKey(word)) {
-        System.out.printf("Contain %s \n", word);
+//        System.out.printf("Contain %s \n", word);
         hMap.put(word, ((hMap.get(word)*10) + 1.0) / 10);
 
       } else {
-        System.out.printf("does not contain word %s \n", word);
+//        System.out.printf("does not contain word %s \n", word);
         hMap.put(word, (0.1));
       }
     }
